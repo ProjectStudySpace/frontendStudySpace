9101a2e593ad408e2c5551cf89b189e9170386e8
Juan Ruiz <jdruiz008@gmail.com>
2026-01-10 03:40:52 -0500

    config update

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,6 @@
 export const API_URL =
   import.meta.env.VITE_API_URL || "https://api.memopal.app/api";
+
+//se mantiene para pruebas locales
+// export const API_URL =
+//   import.meta.env.VITE_API_URL || "http://localhost:3000/api";
